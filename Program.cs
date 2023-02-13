059108da3b98bf509b810250e7c281bae12531b4
Sonu Sanil K <sonu.k@experionglobal.local>
2023-02-13 15:04:22 +0530

    first commit

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Enter two numbers: ");
+int a = int.Parse(Console.ReadLine());
+int b = int.Parse(Console.ReadLine());
+int t = 0; Console.WriteLine($" a = {a} and b = {b}");
+t = a;
+a = b;
+b = t;
+Console.WriteLine("............After swapping............");
+Console.WriteLine($" a = {a} and b = {b}");
+
